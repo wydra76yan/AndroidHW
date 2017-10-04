d5f1e86cf35ad94cb35d391bf909f0782480f3f5
wydra76yan <yan@vydra.com>
2017-10-04 23:45:02 +0300

    After merging commit

---
+++ app/src/main/java/com/example/yanvydra/androidhw/MainActivity_BACKUP_15176.java
@@ -0,0 +1,25 @@
+package com.example.yanvydra.androidhw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initial commit
+    }
+<<<<<<< HEAD
+    //doing conflict with
+    //doing conflict wham
+    //doing conflict wh0m
+    //doing conflict whuuum
+
+=======
+    //doing conflict with him
+    //doing conflict whom
+    //doing conflict wham
+>>>>>>> ebfb8140d3a5673ae6e34ed98c5a9965ddd656a6
+}
---
+++ app/src/main/java/com/example/yanvydra/androidhw/MainActivity_REMOTE_15176.java
@@ -0,0 +1,17 @@
+package com.example.yanvydra.androidhw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //initial commit
+    }
+    //doing conflict with him
+    //doing conflict whom
+    //doing conflict wham
+}

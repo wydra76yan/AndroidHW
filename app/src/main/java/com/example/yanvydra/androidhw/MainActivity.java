15a6507e4b6d534618bd88bd1ffb1fddd3102d33
wydra76yan <yan@vydra.com>
2017-10-04 23:08:03 +0300

    Commit master

--- app/src/main/java/com/example/yanvydra/androidhw/MainActivity.java
+++ app/src/main/java/com/example/yanvydra/androidhw/MainActivity.java
@@ -11,4 +11,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //initial commit
     }
+    //doing conflict with
+    //doing conflict wham
+    //doing conflict wh0m
+    //doing conflict whuuum
+
 }
